2ad1c5b1ee28028d91e548410d764b4e6628f878
Celine Martinet Sanchez <celine_ms@MacBook-Pro-de-Celine.local>
2014-02-17 14:54:56 +0100

    devise v3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :full_name << :telephone
+    devise_parameter_sanitizer.for(:account_update) << :full_name << :telephone
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
-class PagesController < ApplicationController::Base
+class PagesController < ApplicationController
 
+	def home
+
+	end
 
 end
