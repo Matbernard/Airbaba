979aaa820a00ff0348eac45b983178008aebd29c
Edouard <edouard.rudolf@gmail.com>
2014-02-17 09:51:01 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Airbaba::Application.config.secret_key_base = '3885cbf20db9ddd827189529e0968f6bd5b652670b3f97f667577817f7ab58d66809bda15ecebff07dae5fc95177898e9671174d7009320388859ec4841c47d9'
